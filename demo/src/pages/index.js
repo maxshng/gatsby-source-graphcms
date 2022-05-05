5147f20143927076bf65407dc62823f1e6d6bb8a
Max the Max <maxshng@gmail.com>
2022-05-05 12:18:01 +0800

    Update index.js

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -4,6 +4,7 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const IndexPage = ({ data: { products } }) => {
   return (
+    <h1> Max Trial </h1>
     <ul className="gap-6 grid grid-cols-1 max-w-6xl md:grid-cols-3 mx-auto">
       {products.nodes.map((product) => {
         const [mainImage] = product.images

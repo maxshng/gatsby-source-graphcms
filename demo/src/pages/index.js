905c513ebe92a3060915dfcadea93cc0c1591526
Max the Max <maxshng@gmail.com>
2022-05-05 16:42:52 +0800

    Create index.js

---
+++ demo/src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import MainLayout from '../components/mainlayout'
+
+const IndexPage = () => {
+  return (
+    <MainLayout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </MainLayout>
+  )
+}
+
+export default IndexPage
